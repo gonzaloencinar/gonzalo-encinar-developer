cadbf32c57c06c9cce7e379c7a27c2cface2b4d1
Gonzalo Encinar <gonzalo.encinar.contractor@bbva.com>
2024-08-29 00:48:48 +0200

    general config and metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Devolio';
-export const SITE_TAGLINE = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site meta description for SEO
+export const SITE_TITLE = 'Gonzalo Encinar | Software Engineer, Quality Engineer & no code specialist';
+export const SITE_TAGLINE = 'Software Engineer, Quality Engineer & No-Code Expert'; // site tagline
+export const SITE_DESCRIPTION = 'Software Engineer, Quality Engineer & No-Code Expert'; // site meta description for SEO
